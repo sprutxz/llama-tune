1a6fb7d525be2540767bc4e8eb26ef090bf8a5aa
sprutz <leroy.souz@gmail.com>
2025-03-03 16:45:59 -0500

    idk no more

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ class train_config:
 @dataclass
 class fsdp_config:
     mixed_precision: bool = True
-    use_fp16: bool=True
+    use_fp16: bool=False
     sharding_strategy: ShardingStrategy = ShardingStrategy.FULL_SHARD # HYBRID_SHARD "Full Shard within a node DDP cross Nodes", SHARD_GRAD_OP "Shard only Gradients and Optimizer States", NO_SHARD "Similar to DDP".
     hsdp : bool =False # Require HYBRID_SHARD to be set. This flag can extend the HYBRID_SHARD by allowing sharding a model on customized number of GPUs (Sharding_group) and Replicas over Sharding_group.
     sharding_group_size: int=0 # requires hsdp to be set. This specifies the sharding group size, number of GPUs that you model can fit into to form a replica of a model.
@@ -105,7 +105,7 @@ class fsdp_config:
     checkpoint_type: StateDictType = StateDictType.FULL_STATE_DICT  # alternatively FULL_STATE_DICT can be used. SHARDED_STATE_DICT saves one file with sharded weights per rank while FULL_STATE_DICT will collect all weights on rank 0 and save them in a single file.
     fsdp_activation_checkpointing: bool=True
     fsdp_cpu_offload: bool=False
-    pure_bf16: bool = False
+    pure_bf16: bool = True
     optimizer: str= "AdamW"
     
 def resize_image(img, max_dimension = 1120):
@@ -488,8 +488,6 @@ def get_model_and_processor(train_config):
             target_modules=["q_proj", "k_proj", "v_proj", "o_proj"],
             lora_dropout=0.05,
             bias="none",
-            # Add the dtype parameter to ensure LoRA parameters use bfloat16
-            dtype=torch.bfloat16,
         )
         model = get_peft_model(model, peft_config)
         print("Applied PEFT (LoRA) to model")
